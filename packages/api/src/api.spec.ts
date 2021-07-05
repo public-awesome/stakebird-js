16a03ed58971de84c796d5fd09ca982afe874700
Shane Vitarana <shanev@gmail.com>
2021-07-04 22:51:35 -0400

    Removed disabled curating test

--- packages/api/src/api.spec.ts
+++ packages/api/src/api.spec.ts
@@ -33,52 +33,52 @@ describe('StargazeApi', () => {
 		});
 	});
 
-	describe('Txs', () => {
-		it('should post a tx on chain', async () => {
-			const mnemonic =
-				'jacket powder menu denial capital setup result vintage melody mouse innocent start elephant umbrella second monkey saddle matter avoid frost sentence mask matrix anxiety';
-			const wallet = await DirectSecp256k1HdWallet.fromMnemonic(
-				mnemonic,
-				{ prefix: 'stars' }
-			);
-			const [{ address }] = await wallet.getAccounts();
+	// describe('Txs', () => {
+	// 	it('should post a tx on chain', async () => {
+	// 		const mnemonic =
+	// 			'jacket powder menu denial capital setup result vintage melody mouse innocent start elephant umbrella second monkey saddle matter avoid frost sentence mask matrix anxiety';
+	// 		const wallet = await DirectSecp256k1HdWallet.fromMnemonic(
+	// 			mnemonic,
+	// 			{ prefix: 'stars' }
+	// 		);
+	// 		const [{ address }] = await wallet.getAccounts();
 
-			const msgPostTypeUrl = '/stargaze.curating.v1beta1.MsgPost';
-			const registry = new Registry();
-			registry.register(msgPostTypeUrl, MsgPost);
+	// 		const msgPostTypeUrl = '/stargaze.curating.v1beta1.MsgPost';
+	// 		const registry = new Registry();
+	// 		registry.register(msgPostTypeUrl, MsgPost);
 
-			const client = await SigningStargateClient.connectWithSigner(
-				chainUrl,
-				wallet,
-				{ registry: registry }
-			);
+	// 		const client = await SigningStargateClient.connectWithSigner(
+	// 			chainUrl,
+	// 			wallet,
+	// 			{ registry: registry }
+	// 		);
 
-			const msg = MsgPost.fromPartial({
-				vendorId: 1,
-				postId: '1',
-				creator: address,
-				rewardAccount: address,
-				body: 'This is a tweet',
-			});
+	// 		const msg = MsgPost.fromPartial({
+	// 			vendorId: 1,
+	// 			postId: '1',
+	// 			creator: address,
+	// 			rewardAccount: address,
+	// 			body: 'This is a tweet',
+	// 		});
 
-			const msgAny = {
-				typeUrl: msgPostTypeUrl,
-				value: msg,
-			};
+	// 		const msgAny = {
+	// 			typeUrl: msgPostTypeUrl,
+	// 			value: msg,
+	// 		};
 
-			const fee = {
-				amount: coins(10000000, 'ustarx'),
-				gas: '180000', // 180k
-			};
-			const memo = 'Use your power wisely';
-			const result = await client.signAndBroadcast(
-				address,
-				[msgAny],
-				fee,
-				memo
-			);
+	// 		const fee = {
+	// 			amount: coins(10000000, 'ustarx'),
+	// 			gas: '180000', // 180k
+	// 		};
+	// 		const memo = 'Use your power wisely';
+	// 		const result = await client.signAndBroadcast(
+	// 			address,
+	// 			[msgAny],
+	// 			fee,
+	// 			memo
+	// 		);
 
-			assertIsBroadcastTxSuccess(result);
-		});
-	});
+	// 		assertIsBroadcastTxSuccess(result);
+	// 	});
+	// });
 });
