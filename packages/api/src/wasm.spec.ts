efa53f4262e40086d4a2f3f65d2cfde9f248009d
Shane Vitarana <shanev@gmail.com>
2021-07-04 20:49:53 -0400

    Add cosmwasm package

--- packages/api/src/wasm.spec.ts
+++ packages/api/src/wasm.spec.ts
@@ -1,2 +1,22 @@
-import { DirectSecp256k1HdWallet, Registry } from '@cosmjs/proto-signing';
+// import { DirectSecp256k1HdWallet, Registry } from '@cosmjs/proto-signing';
+
+// import {
+//     alice,
+//     defaultClearAdminFee,
+//     defaultExecuteFee,
+//     defaultInstantiateFee,
+//     defaultMigrateFee,
+//     defaultSendFee,
+//     defaultUpdateAdminFee,
+//     defaultUploadFee,
+//     getHackatom,
+//     makeRandomAddress,
+//     makeWasmClient,
+//     ModifyingDirectSecp256k1HdWallet,
+//     ModifyingSecp256k1HdWallet,
+//     pendingWithoutWasmd,
+//     unused,
+//     validator,
+//     wasmd,
+//   } from "./testutils.spec";
 
